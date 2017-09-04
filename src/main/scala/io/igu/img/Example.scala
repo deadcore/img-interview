7cb1b890cb19698fd2d3703bb0fdab4e73caa62c
Jack Liddiard <jack.liddiard@iiincubator.com>
2017-09-04 08:16:18 +0100

    Tidy up the banners

--- src/main/scala/io/igu/img/Example.scala
+++ src/main/scala/io/igu/img/Example.scala
@@ -6,17 +6,24 @@ object Example extends App {
     println("********************************************")
     println("***** Received a whole new match state *****")
     println("********************************************")
+    println()
 
     // The last event (i.e. which team last scored, how many points, at what point through the match and what the resulting match score was)
     println(s"The last event was ${matchState.last}")
     println(s"The latest event was ${matchState.latest}")
 
     // The last n events (where 0 <= n <= Total Items)
-    println(s"The last n number of events were ${matchState.slice(0, 3)}")
+    println(s"The first three events are ${matchState.slice(0, 3)}")
+
+    println()
+
+    println("****** All events in the match so far ******")
+    println()
 
     // All events in the match so far
     matchState.foreach(println)
 
-    println("********************************************")
+    println()
+
   }
 }
